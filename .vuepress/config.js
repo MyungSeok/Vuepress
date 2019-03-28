fa51f7018dcbfeea7069b3c6267188fbe04e4f10
김명석 <rrest@wemakeprice.com>
2019-03-28 17:49:32 +0900

    no message

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,34 +8,18 @@ module.exports = {
     '@vuepress/back-to-top'
   ],
   themeConfig: {
-    sidebar: {
-      '/about/': [
-        '/about/install',
-        '/about/me',
-        '/about/syntax'
-      ]
-    },
     nav: [
-      { text: 'About', link: '/about/',
-        items: [
-          {
-            text: 'Group 1', 
-            items: [
-              { text: 'Installation', link: '/about/install'}
-            ]
-          },
-          {
-            text: 'Group 2', 
-            items: [
-              { text: 'About Me', link: '/about/me' },
-              { text: 'Syntax', link: '/about/syntax' }
-            ]
-          }
-        ]
-      },
+      { text: 'About', link: '/about/'},
       { text: 'General', link: '/general/' },
       { text: 'Front End', link: '/frontend/' },
       { text: 'Back End', link: '/backend/' },
-    ]
+    ],
+    sidebar: {
+      '/about/': [
+        '',
+        '/about/install',
+        '/about/syntax'
+      ]
+    }
   }
 };
