f9d9ca39e86ee66f17fccf579b77dbee0022b90d
rrest <rrest@icloud.com>
2019-05-20 12:44:22 +0900

    no message

--- publish_vuepress.sh
+++ publish_vuepress.sh
@@ -3,7 +3,23 @@
 # abort on errors
 # set -e
 
-npm install && vuepress build
+# npm install && vuepress build
+
+# git checkout master
+
+# git pull origin master --rebase
+
+# cp -R .vuepress/dist/* .
+
+# git add .
+
+# git commit -a -m 'Publish Docs'
+
+# git push origin master
+
+# git checkout draft
+
+vuepress build
 
 git checkout master
 
